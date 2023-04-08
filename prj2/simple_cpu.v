6a99b0d6459f2070e6c0badedccab5f9a5e23489
Flynn <89886971+FlynnHHH@users.noreply.github.com>
2023-04-08 20:08:00 +0800

    Create simple_cpu.v

---
+++ prj2/simple_cpu.v
@@ -0,0 +1,31 @@
+`timescale 10ns / 1ns
+
+module simple_cpu(
+	input             clk,
+	input             rst,
+
+	output [31:0]     PC,
+	input  [31:0]     Instruction,
+
+	output [31:0]     Address,
+	output            MemWrite,
+	output [31:0]     Write_data,
+	output [ 3:0]     Write_strb,
+
+	input  [31:0]     Read_data,
+	output            MemRead
+);
+
+	// THESE THREE SIGNALS ARE USED IN OUR TESTBENCH
+	// PLEASE DO NOT MODIFY SIGNAL NAMES
+	// AND PLEASE USE THEM TO CONNECT PORTS
+	// OF YOUR INSTANTIATION OF THE REGISTER FILE MODULE
+	wire			RF_wen;
+	wire [4:0]		RF_waddr;
+	wire [31:0]		RF_wdata;
+
+	// TODO: PLEASE ADD YOUR CODE BELOW
+	always @(posedge clk ) begin
+		
+	end
+endmodule
